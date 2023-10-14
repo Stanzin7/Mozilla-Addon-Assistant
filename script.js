f668d809a856ac643f33680d8a594522201bfef6
Stanzin7 <Stanzin.norzang@wartburg.edu>
2023-10-13 19:36:38 -0500

    Chatbot integration

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var script = document.createElement("script");
+  script.src = "https://fiverr.vortext.ca/vtbot.js?p=6620";
+  script.setAttribute("default-bot", "norzang");
+  script.setAttribute("id", "vtscriptalias");
+  script.setAttribute("data-name", "fiverr");
+  script.setAttribute("async", true);
+  document.body.appendChild(script);
+});
