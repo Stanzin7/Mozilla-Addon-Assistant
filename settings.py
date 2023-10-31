c8d91bfda1e4b0b4dd6dc6c21f72b2992556ca57
Stanzin7 <Stanzin.norzang@wartburg.edu>
2023-10-31 01:37:00 -0500

    Added HLD.md

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 import os
 
-os.environ["OPENAI_API_KEY"] = "YOUR_API_KEY_HERE"
+os.environ["OPENAI_API_KEY"] = "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"
 APIKEY = os.environ["OPENAI_API_KEY"]
